6eb82e6e5ee4939e6b3603b143e3b62e45d97a6e
daje0601 <englishmt4118@gmail.com>
2020-12-05 14:00:29 +0900

    update

---
+++ todo.js
@@ -0,0 +1,159 @@
+
+const toDoForm = document.querySelector(".todo__form"),
+toDoInput = toDoForm.querySelector("input"),
+pendingList = document.querySelector(".pending__list"),
+finishedList = document.querySelector(".finished__list");
+
+const PENDING = "pending";
+const FINISHED = "finished";
+let pending = [];
+let finished = [];
+
+function deleteToDo(event) {
+const btn = event.target;
+const li = btn.parentNode;
+pendingList.removeChild(li);
+const cleanToDos = pending.filter(function (toDo) {
+  return toDo.id !== parseInt(li.id);
+});
+pending = cleanToDos;
+saveToDos();
+}
+
+function deleteFinished(event) {
+const btn = event.target;
+const li = btn.parentNode;
+finishedList.removeChild(li);
+const cleanToDos = pending.filter(function (fin) {
+  return fin.id !== parseInt(li.id);
+});
+finished = cleanToDos;
+saveToDos();
+}
+
+function moveToFinishedList(event) {
+const btn = event.target;
+const li = btn.parentNode;
+pendingList.removeChild(li);
+
+const cleanToDos = pending.filter(function (toDo) {
+  return toDo.id !== parseInt(li.id);
+});
+pending = cleanToDos;
+saveToDos();
+
+let text = li.querySelector("span").innerHTML;
+paintFinished(text);
+
+const delFins = finished.filter(function (fin) {
+  return fin.id !== parseInt(li.id);
+});
+finished = delFins;
+saveToDos();
+}
+
+function moveToPendingList(event) {
+const btn = event.target;
+const li = btn.parentNode;
+finishedList.removeChild(li);
+  
+const cleanToDos = pending.filter(function (toDo) {
+  return toDo.id !== parseInt(li.id);
+});
+pending = cleanToDos;
+saveToDos();
+
+let text = li.querySelector("span").innerHTML;
+paintPending(text);
+
+const delFins = finished.filter(function (fin) {
+  return fin.id !== parseInt(li.id);
+});
+finished = delFins;
+saveToDos();
+}
+
+function saveToDos() {
+localStorage.setItem(PENDING, JSON.stringify(pending));
+localStorage.setItem(FINISHED, JSON.stringify(finished));
+}
+
+function paintPending(text) {
+const li = document.createElement("li");
+const delBtn = document.createElement("button");
+const downBtn = document.createElement("button");
+const span = document.createElement("span");
+const newId = pending.length + 1;
+downBtn.innerText = "v";
+downBtn.addEventListener("click", moveToFinishedList);
+delBtn.innerText = "x";
+delBtn.addEventListener("click", deleteToDo);
+span.innerText = text;
+li.appendChild(delBtn);
+li.appendChild(downBtn);
+li.appendChild(span);
+li.id = newId;
+pendingList.appendChild(li);
+const toDoObj = {
+  text: text,
+  id: newId
+};
+pending.push(toDoObj);
+saveToDos(PENDING, pending);
+}
+
+function paintFinished(text, event) {
+const li = document.createElement("li");
+const delBtn = document.createElement("button");
+const downBtn = document.createElement("button");
+const span = document.createElement("span");
+const newId = finished.length + 1;
+downBtn.innerText = "B";
+downBtn.addEventListener("click", moveToPendingList);
+delBtn.innerText = "x";
+delBtn.addEventListener("click", deleteFinished);
+span.innerText = text;
+li.appendChild(delBtn);
+li.appendChild(downBtn);
+li.appendChild(span);
+li.id = newId;
+finishedList.appendChild(li);
+const toDoObj = {
+  text: text,
+  id: newId
+};
+pending.push(toDoObj);
+saveToDos(FINISHED, finished);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+const currentValue = toDoInput.value;
+paintPending(currentValue);
+toDoInput.value = "";
+}
+
+function loadToDos() {
+const loadedPending = localStorage.getItem(PENDING);
+const loadedFinished = localStorage.getItem(FINISHED);
+
+if (loadedFinished !== null) {
+  const parsedFinished = JSON.parse(loadedFinished);
+  parsedFinished.forEach(function (toDo) {
+    paintFinished(toDo.text);
+  });
+}
+if (loadedPending !== null) {
+  const parsedPending = JSON.parse(loadedPending);
+  parsedPending.forEach(function (toDo) {
+    paintPending(toDo.text);
+  });
+}
+}
+
+function init() {
+loadToDos();
+toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
